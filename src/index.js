5070bd4178c89d40e286a85d5fdca672409325f4
MC3D <m.chapman.home@gmail.com>
2016-11-26 15:45:25 -0500

    renders SearchBar component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react'; // used to create a manage components
 import ReactDOM from 'react-dom'; // used to interact with the DOM
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyBMwwCV_8u6YefEUZoVyc__gSJwtWUYVDM';
 
 // Create a new component. This component should produce
 // some HTML.
 
 const App = () => { // App is a class, not an instance
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it
